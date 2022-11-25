76376c71bb2401197601c0941e525ed569b2f0fa
unknown <anees7860a@gmail.com>
2022-11-25 06:40:15 -0800

    fetch data

---
+++ javascript.js
@@ -0,0 +1,35 @@
+const api_url="https://jsonplaceholder.typicode.com/posts";
+ async function getapi(url){
+
+    const response=await fetch(url);
+    var data=await response.json();
+    console.log(data);
+    if(response){
+        hideloader();
+    }
+    show(data);
+ }
+ getapi(api_url);
+
+ function hideloader(){
+    document.getElementById("loading").style.display="none";
+ }
+  function show(data){
+    let tab = 
+        `<tr>
+         <th>userId</th>
+          <th>id</th>
+          <th>title</th>
+          <th>body</th>
+         </tr>`;
+
+         for(let r of data.data){
+            tab +=`<tr>
+            <td>${r.userId}
+            <td>${r.id}</td>
+            <td>${r.title}</td>
+            <td>${r.body}</td>
+            </tr>`;
+         }
+         document.getElementById("tab1").innerHTML = tab;
+ }
